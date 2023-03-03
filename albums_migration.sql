4bfb053b8356ff7d98b3d78c9a4b3e4a598b3b32
Hung Ho <hung.ho902@gmail.com>
2023-03-03 15:27:24 -0600

    feat:

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,13 @@
 USE codeup_test_db;
+
+    drop table albums;
+
     CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(100) NOT NULL,
-    name VARCHAR(100),
-    release_date DATE,
+    artist VARCHAR(128) NOT NULL,
+    name VARCHAR(128),
+    release_date SMALLINT UNSIGNED,
     sales FLOAT,
-    genre VARCHAR(46),
+    genre VARCHAR(128),
     PRIMARY KEY (id)
     );
